899d4db679a6895aa08ca0d70bad32bf8dbd3a56
Matt McVey <matt.mcvey49@gmail.com>
2021-04-26 15:56:29 -0600

    Add responsive design and update icon in browser tab

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Nav.css'
-import spiderman from './spiderman.svg'
+import spiderman from '../../../src/spiderman.svg'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <header>
-      <div className='title-container'>
+      <div className='title-container hidden'>
         <h2>HERO FINDER</h2>
         <p>WHERE VILLAINS FIND THEIR FOREVER ENEMY</p>
       </div>
@@ -14,7 +14,7 @@ const Nav = () => {
         <img className='logo' src={spiderman} alt='logo'></img>
       </Link>
       <Link to='/favorites' className='button-container'>
-        <button className='button'>Future Archenemies</button>
+        <button className='button'>Archenemies</button>
       </Link>
     </header>
   )
